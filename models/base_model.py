e70d71b65da8c07fc955edf4b61d3b255998a47f
mohamdii <m.hamdy97@gmail.com>
2024-03-07 16:51:33 +0200

    4th task

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,19 @@ from datetime import datetime
 
 class BaseModel:
 
-    def __init__(self):
-       self.id = str(uuid.uuid4())
-       self.created_at = datetime.now()
-       self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return self.__class__.__name__ + "({}) {}".format(self.id, self.__dict__)
+        return "[{}] ".format(self.__class__.__name__) + "({}) {}".format(self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
